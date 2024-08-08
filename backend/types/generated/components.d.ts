048c4f4d237c40b3ce2e6641a144a8856b9ab5ad
tranhuuphucuong <tranhuu.phucuong@gmail.com>
2024-08-08 20:06:33 +0700

    Update packages

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,27 +1,84 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
-export interface MetaMetadata extends Schema.Component {
-  collectionName: 'components_meta_metadata';
+export interface WidgetBio extends Schema.Component {
+  collectionName: 'components_widget_bios';
   info: {
-    displayName: 'metadata';
-    icon: 'database';
+    displayName: 'bio';
+    icon: 'alien';
+    description: '';
   };
   attributes: {
     title: Attribute.String;
     description: Attribute.String;
+    avatar: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
   };
 }
 
-export interface SectionsFeatureVideo extends Schema.Component {
-  collectionName: 'components_sections_feature_videos';
+export interface SharedSocialLink extends Schema.Component {
+  collectionName: 'components_widget_social_links';
   info: {
-    displayName: 'featureVideo';
-    icon: 'cube';
+    displayName: 'socialLink';
+    icon: 'earth';
+    description: '';
   };
   attributes: {
     title: Attribute.String;
-    Description: Attribute.String;
-    video: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    url: Attribute.String;
+    icon: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    platform: Attribute.Enumeration<
+      ['facebook', 'youtube', 'x', 'linkedin', 'instagram']
+    >;
+  };
+}
+
+export interface SharedSocialLinkList extends Schema.Component {
+  collectionName: 'components_widget_social_link_lists';
+  info: {
+    displayName: 'socialLinkList';
+    icon: 'earth';
+    description: '';
+  };
+  attributes: {
+    SocialLink: Attribute.Component<'shared.social-link', true>;
+  };
+}
+
+export interface SharedSeo extends Schema.Component {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'seo';
+    description: '';
+  };
+  attributes: {
+    metaTitle: Attribute.String;
+    metaDescription: Attribute.Text;
+    sharedImage: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+  };
+}
+
+export interface SharedLink extends Schema.Component {
+  collectionName: 'components_shared_links';
+  info: {
+    displayName: 'link';
+    icon: 'earth';
+  };
+  attributes: {
+    url: Attribute.String;
+    text: Attribute.String;
+    newTab: Attribute.Boolean;
+  };
+}
+
+export interface SharedButton extends Schema.Component {
+  collectionName: 'components_shared_buttons';
+  info: {
+    displayName: 'button';
+    icon: 'cursor';
+  };
+  attributes: {
+    text: Attribute.String;
+    type: Attribute.Enumeration<['solid', 'outline']>;
+    color: Attribute.Enumeration<['primary', 'secondary', 'danger']>;
   };
 }
 
@@ -43,100 +100,43 @@ export interface SectionsHero extends Schema.Component {
   };
 }
 
-export interface SharedButton extends Schema.Component {
-  collectionName: 'components_shared_buttons';
+export interface SectionsFeatureVideo extends Schema.Component {
+  collectionName: 'components_sections_feature_videos';
   info: {
-    displayName: 'button';
-    icon: 'cursor';
-  };
-  attributes: {
-    text: Attribute.String;
-    type: Attribute.Enumeration<['solid', 'outline']>;
-    color: Attribute.Enumeration<['primary', 'secondary', 'danger']>;
-  };
-}
-
-export interface SharedLink extends Schema.Component {
-  collectionName: 'components_shared_links';
-  info: {
-    displayName: 'link';
-    icon: 'earth';
-  };
-  attributes: {
-    url: Attribute.String;
-    text: Attribute.String;
-    newTab: Attribute.Boolean;
-  };
-}
-
-export interface SharedSeo extends Schema.Component {
-  collectionName: 'components_shared_seos';
-  info: {
-    displayName: 'seo';
-    description: '';
-  };
-  attributes: {
-    metaTitle: Attribute.String;
-    metaDescription: Attribute.Text;
-    sharedImage: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
-  };
-}
-
-export interface SharedSocialLinkList extends Schema.Component {
-  collectionName: 'components_widget_social_link_lists';
-  info: {
-    displayName: 'socialLinkList';
-    icon: 'earth';
-    description: '';
-  };
-  attributes: {
-    SocialLink: Attribute.Component<'shared.social-link', true>;
-  };
-}
-
-export interface SharedSocialLink extends Schema.Component {
-  collectionName: 'components_widget_social_links';
-  info: {
-    displayName: 'socialLink';
-    icon: 'earth';
-    description: '';
+    displayName: 'featureVideo';
+    icon: 'cube';
   };
   attributes: {
     title: Attribute.String;
-    url: Attribute.String;
-    icon: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
-    platform: Attribute.Enumeration<
-      ['facebook', 'youtube', 'x', 'linkedin', 'instagram']
-    >;
+    Description: Attribute.String;
+    video: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
   };
 }
 
-export interface WidgetBio extends Schema.Component {
-  collectionName: 'components_widget_bios';
+export interface MetaMetadata extends Schema.Component {
+  collectionName: 'components_meta_metadata';
   info: {
-    displayName: 'bio';
-    icon: 'alien';
-    description: '';
+    displayName: 'metadata';
+    icon: 'database';
   };
   attributes: {
     title: Attribute.String;
     description: Attribute.String;
-    avatar: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
   };
 }
 
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
-      'meta.metadata': MetaMetadata;
-      'sections.feature-video': SectionsFeatureVideo;
-      'sections.hero': SectionsHero;
-      'shared.button': SharedButton;
-      'shared.link': SharedLink;
-      'shared.seo': SharedSeo;
-      'shared.social-link-list': SharedSocialLinkList;
-      'shared.social-link': SharedSocialLink;
       'widget.bio': WidgetBio;
+      'shared.social-link': SharedSocialLink;
+      'shared.social-link-list': SharedSocialLinkList;
+      'shared.seo': SharedSeo;
+      'shared.link': SharedLink;
+      'shared.button': SharedButton;
+      'sections.hero': SectionsHero;
+      'sections.feature-video': SectionsFeatureVideo;
+      'meta.metadata': MetaMetadata;
     }
   }
 }
